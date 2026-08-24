abbf7a9e0842092ec44b59059433103ad43920d4
mansour-az <123008837+mansour-az@users.noreply.github.com>
2026-08-24 06:56:34 +0330

    Refine Venzo red glass day and night theme

--- venzo-windows/venzo_theme.go
+++ venzo-windows/venzo_theme.go
@@ -33,14 +33,32 @@ func ApplyVenzoTheme(application fyne.App, dark bool) {
 func (t *venzoTheme) Color(name fyne.ThemeColorName, variant fyne.ThemeVariant) color.Color {
 	switch name {
 	case theme.ColorNamePrimary:
-		return color.NRGBA{R: 215, G: 25, B: 32, A: 255}
+		return color.NRGBA{R: 255, G: 31, B: 53, A: 255}
 	case theme.ColorNameFocus:
-		return color.NRGBA{R: 239, G: 35, B: 60, A: 255}
+		return color.NRGBA{R: 255, G: 71, B: 87, A: 255}
 	case theme.ColorNameSelection:
-		return color.NRGBA{R: 215, G: 25, B: 32, A: 112}
+		return color.NRGBA{R: 255, G: 31, B: 53, A: 112}
+	case theme.ColorNameBackground:
+		if variant == theme.VariantDark {
+			return color.NRGBA{R: 9, G: 14, B: 26, A: 255}
+		}
+	case theme.ColorNameMenuBackground, theme.ColorNameInputBackground:
+		if variant == theme.VariantDark {
+			return color.NRGBA{R: 17, G: 24, B: 39, A: 255}
+		}
+	case theme.ColorNameSeparator:
+		if variant == theme.VariantDark {
+			return color.NRGBA{R: 58, G: 30, B: 36, A: 255}
+		}
+	case theme.ColorNameSuccess:
+		return color.NRGBA{R: 0, G: 230, B: 118, A: 255}
+	case theme.ColorNameWarning:
+		return color.NRGBA{R: 255, G: 179, B: 0, A: 255}
+	case theme.ColorNameError:
+		return color.NRGBA{R: 255, G: 71, B: 87, A: 255}
 	default:
-		return t.base.Color(name, variant)
 	}
+	return t.base.Color(name, variant)
 }
 
 func (t *venzoTheme) Font(style fyne.TextStyle) fyne.Resource {
